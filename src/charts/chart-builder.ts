7d6eb9a7629d5a6bc938c655ad5c5a16dbec07eb
Gabriel Dias <dias.1618@gmail.com>
2023-12-29 14:30:30 -0300

    fix/colors-chart

--- src/charts/chart-builder.ts
+++ src/charts/chart-builder.ts
@@ -4,7 +4,6 @@ import { PizzaChart } from "./pizza-chart";
 import { ManagerModalChart } from "./manager-modal-chart";
 import { IChartData } from "./chart-data";
 import Chart from 'chart.js';
-import { MesLabels } from "./mes.enum";
 import { tipoGrafico } from "./tipo-grafico.enum";
 import { IChartOptions } from "./chart-options";
 export class ChartBuilder{
@@ -44,74 +43,11 @@ export class ChartBuilder{
         return this;
     }
 
-    private _mensal():ChartBuilder{
-        let campoData:string = this._formGroup.value.campoAnalisado.dataField;
-        let map:Map<string, number> = new Map();
-        for(let item of this._items){
-            let date:Date = new Date(item[campoData]);
-            let mesAno = `${MesLabels.get(date.getMonth())}/${date.getFullYear()}`;
-            if(map.has(mesAno))
-                map.set(mesAno, map.get(mesAno) + 1);
-            else
-                map.set(mesAno, 1);
-        }
-        this._dataMensal = this._createData(map);
-        return this;
-    }
-
-    private _porHora():ChartBuilder{
-        let campoHora:string = this._formGroup.value.campoAnalisado.dataField;
-        let map:Map<string, number> = new Map();
-        for(let item of this._items){
-            let date:Date = new Date(item[campoHora]);
-            let hora = `${date.getHours()}:00`;
-            if(map.has(hora))
-                map.set(hora, map.get(hora) + 1);
-            else
-                map.set(hora, 1);
-        }
-
-        let mapOrdenado:Map<any, number> = new Map();
-        while(map.size > 0){
-            let maiorKey:string = null;
-            for(let key of map.keys()){
-                if(maiorKey == null || new Number(key.split(':')[0]) > new Number(maiorKey.split(':')[0])){
-                    maiorKey = key;
-                }
-            }
-            mapOrdenado.set(maiorKey, map.get(maiorKey));
-            map.delete(maiorKey);
-        }
-        map = mapOrdenado;
-        
-        this._dataPorHora = this._createData(map);
-        return this;
-    }
-
-    private _createData(map:Map<string, number>):IChartData{
-        let labels:any[] = [];
-        let dataSet:any[] = [];
-        for(let key of map.keys()){
-            labels.push(key);
-            dataSet.push(map.get(key));
-        }
-
-        return {
-            labels: labels,
-            datasets: [{ 
-                data: dataSet,
-                backgroundColor: "rgba(75, 192, 192, 0.2)",
-                borderColor: "rgba(75, 192, 192, 1)",
-                borderWidth: 1
-            }],
-        };
-    }
-
     build():Chart{
         if(this._formGroup && this._formGroup.value.campoAnalisado.type == 'date')
-            this._mensal();
+            this._dataMensal = this._managerModalChart.mensal(this._formGroup, this._items);
         if(this._formGroup && this._formGroup.value.campoAnalisado.type == 'hour')
-            this._porHora();
+            this._dataPorHora = this._managerModalChart.porHora(this._formGroup, this._items);
         switch(this._tpGrafico){
             case tipoGrafico.BARRAS:
                 return new BarChart(this._chartOptions || {
--- src/charts/manager-modal-chart.ts
+++ src/charts/manager-modal-chart.ts
@@ -1,7 +1,21 @@
+import { FormGroup } from "@angular/forms";
 import { FilterSearch } from "../reporter/filter-search";
 import { IChartData } from "./chart-data";
+import { MesLabels } from "./mes.enum";
 
 export class ManagerModalChart{
+
+    private _COLORS = [
+        '#4dc9f6',
+        '#f67019',
+        '#f53794',
+        '#537bc4',
+        '#acc236',
+        '#166a8f',
+        '#00a950',
+        '#58595b',
+        '#8549ba'
+    ];
 
     generateFieldFilters(modal:{listSearch:FilterSearch[], values:any}):void{
         let fieldFilters = "";
@@ -76,29 +90,77 @@ export class ManagerModalChart{
             dataSet.push(map.get(key));
         }
 
-        const COLORS = [
-            '#4dc9f6',
-            '#f67019',
-            '#f53794',
-            '#537bc4',
-            '#acc236',
-            '#166a8f',
-            '#00a950',
-            '#58595b',
-            '#8549ba'
-        ];
-
         const data:IChartData = {
             labels: labels,
             datasets: [{ 
                 data: dataSet,
-                backgroundColor: COLORS,
-                borderColor: "rgba(75, 192, 192, 1)",
+                backgroundColor: this._COLORS,
                 borderWidth: 1
             }],
         };
 
         return data;
     }
+
+    mensal(formGroup:FormGroup, items:any[]):IChartData{
+        let campoData:string = formGroup.value.campoAnalisado.dataField;
+        let map:Map<string, number> = new Map();
+        for(let item of items){
+            let date:Date = new Date(item[campoData]);
+            let mesAno = `${MesLabels.get(date.getMonth())}/${date.getFullYear()}`;
+            if(map.has(mesAno))
+                map.set(mesAno, map.get(mesAno) + 1);
+            else
+                map.set(mesAno, 1);
+        }
+        return this._createData(map);
+    }
+
+    porHora(formGroup:FormGroup, items:any[]):IChartData{
+        let campoHora:string = formGroup.value.campoAnalisado.dataField;
+        let map:Map<string, number> = new Map();
+        for(let item of items){
+            let date:Date = new Date(item[campoHora]);
+            let hora = `${date.getHours()}:00`;
+            if(map.has(hora))
+                map.set(hora, map.get(hora) + 1);
+            else
+                map.set(hora, 1);
+        }
+
+        let mapOrdenado:Map<any, number> = new Map();
+        while(map.size > 0){
+            let maiorKey:string = null;
+            for(let key of map.keys()){
+                if(maiorKey == null || new Number(key.split(':')[0]) > new Number(maiorKey.split(':')[0])){
+                    maiorKey = key;
+                }
+            }
+            mapOrdenado.set(maiorKey, map.get(maiorKey));
+            map.delete(maiorKey);
+        }
+        map = mapOrdenado;
+        
+        return this._createData(map);
+    }
+
+    private _createData(map:Map<string, number>):IChartData{
+        let labels:any[] = [];
+        let dataSet:any[] = [];
+        for(let key of map.keys()){
+            labels.push(key);
+            dataSet.push(map.get(key));
+        }
+
+        return {
+            labels: labels,
+            datasets: [{ 
+                data: dataSet,
+                backgroundColor: this._COLORS,
+                borderWidth: 1
+            }],
+        };
+    }
+
 
 }
